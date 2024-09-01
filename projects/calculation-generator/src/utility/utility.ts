80921dcaa06b079d7553728acba00ec3ab410364
Alexander Bassov <blackxes.dev@gmail.com>
2024-09-01 15:53:14 +0200

    Formatting

--- projects/calculation-generator/src/utility/utility.ts
+++ projects/calculation-generator/src/utility/utility.ts
@@ -7,5 +7,4 @@ export const getRandomFloat = (min: number, max: number) => {
   return Math.random() * (max - min) + min;
 };
 
-export const getRandom = (min: number, max: number) =>
-  Math.floor(getRandomFloat(min, max));
+export const getRandom = (min: number, max: number) => Math.floor(getRandomFloat(min, max));
